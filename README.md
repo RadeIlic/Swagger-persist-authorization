4e52e82c2790ab4bd5317d5a5325ab6656b80cee
rilic <ilicrade13@gmail.com>
2024-04-28 23:03:44 +0200

    Script and documentation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Swagger-persist-authorization
-Swagger persist authorization tampermonkey script
+# Swagger Persist Authorization Tampermonkey Script
+
+This Tampermonkey script is designed to modify the Swagger page settings to persist authorization data across sessions for any domain under `*.your-swagger-page.com`. This script is useful when direct modifications to Swagger UI configuration code is not an option.
+
+## Installation
+
+1. Ensure Tampermonkey is installed in your browser.
+2. Create a new script in Tampermonkey.
+3. Copy and paste the `Swagger-persist-authorization.js` content into the script editor.
---
+++ Swagger-persist-authorization.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Swagger persist authorization
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-26
+// @description  Persist swagger authorization
+// @author       You
+// @match        *://*.your-swagger-page.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var oldOnload = window.onload;
+
+    var oldOnloadText = oldOnload ? oldOnload.toString() : '';
+
+    var modifiedOnloadText = oldOnloadText.replace('"persistAuthorization":false', '"persistAuthorization":true');
+    modifiedOnloadText = modifiedOnloadText.slice(modifiedOnloadText.indexOf('{') + 1, modifiedOnloadText.lastIndexOf('}'));
+    var modifiedOnloadFunction = new Function(modifiedOnloadText);
+
+    window.onload = function() {
+    modifiedOnloadFunction();
+};
+
+})();
